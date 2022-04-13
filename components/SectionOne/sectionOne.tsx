0872e289f8fd321e8444d96f8241624b658f9af3
a-amato93 <aaron@downforce.tech>
2022-04-13 17:08:03 +0100

    added new images

--- components/SectionOne/sectionOne.tsx
+++ components/SectionOne/sectionOne.tsx
@@ -1,29 +1,23 @@
-import FeatureCard from '@components/FeatureCard/FeatureCard';
 import Image from 'next/image';
 
 const SectionOne = () => {
 	function sectionOne(): JSX.Element {
 		return (
-			<div className='grid grid-flow-row-dense grid-cols-3 grid-rows-1 bg-gray-100'>
-				<div className='col-span-1'>
+			<div className='grid grid-flow-row-dense grid-cols-3 grid-rows-1 bg-gray-100 '>
+				<div className='col-span-1 h-978px'>
 					<div className='ml-12  grid grid-flow-row-dense grid-cols-2 grid-rows-2 '>
 						<Image
-							className=''
+							className='overflow-visible	'
 							src='/icons/Group274.svg'
-							height={140}
+							height={220}
 							width={0}
 						></Image>
 					</div>
 
 					<div className='ml-12'>
 						<h1 className='text-2xl primary-font-bold text-left is-purple-one'>Car leasing made</h1>
-						<h1 className='mt-6 text-2xl primary-font-bold text-left is-purple-one'>More simple</h1>
+						<h1 className='mt-1 text-2xl primary-font-bold text-left is-purple-one'>More simple</h1>
 						<div className='mt-6 text-1xl primary-font text-left is-grey-one'>A shiny motor, all the maintenance and no lengthy contracts at an unbeatable monthly rate</div>
-						<br />
-						<textarea
-							name="message"
-							placeholder="Enter message"
-						/>
 					</div>
 
 				</div>
--- components/SectionThree/sectionThree.tsx
+++ components/SectionThree/sectionThree.tsx
@@ -1,59 +1,75 @@
 import FeatureCard from '@components/FeatureCard/FeatureCard';
+import Image from 'next/image';
 
 const SectionThree = () => {
 	function sectionOne(): JSX.Element {
 		return (
-			<div className='flex flex-col justify-center items-center'>
-				<div className='h-80 w-4/5 mx-auto mb-2 p4 rounded-2xl bg-gradient-to-r from-blue-300 to-green-300'></div>
-
-				<div className='my-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
+			<div className='flex flex-row justify-center items-center'>
+				<div className='h-96 w-3/5 mx-auto mb-2 p4 rounded-2xl bg-gradient-to-r from-blue-300 to-green-300'>
+					<div className='mx-3 my-3' style={{ width: '95%', height: '94%', position: 'relative', borderRadius: '10px', overflow: 'hidden' }}>
+						<Image className='object-none' src="/icons/car-image.jpeg" layout='fill'
+							objectFit='cover' />
+					</div>
 				</div>
 
-				<div className='mb-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
+				<div className='px-8'>
+					<div className='my-4'>
+						<FeatureCard
+							title='Unbeaten On Price'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
+
+					<div className='mb-4'>
+						<FeatureCard
+							title='Supreme car quality guarantee'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
+
+					<div className='mb-4'>
+						<FeatureCard
+							title='6 month contracts for piece of mind'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
 				</div>
 
-				<div className='mb-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
-				</div>
 			</div>
 		);
 	}
 
 	function sectionTwo(): JSX.Element {
 		return (
-			<div className='flex flex-col justify-center items-center'>
-				<div className='h-80 w-4/5 mx-auto my-2 p4 rounded-2xl bg-gradient-to-r from-purple-300 to-red-300'></div>
+			<div className='flex flex-row justify-center items-center'>
+				<div>
+					<div className='my-4'>
+						<FeatureCard
+							title='No hefty deposits'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
 
-				<div className='my-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
+					<div className='mb-4'>
+						<FeatureCard
+							title='Save more, the longer you stay with us'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
+
+					<div className='mb-4'>
+						<FeatureCard
+							title='Full convenience'
+							content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
+						/>
+					</div>
 				</div>
 
-				<div className='mb-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
-				</div>
-
-				<div className='mb-4'>
-					<FeatureCard
-						title='Unbeaten On Price'
-						content='We put user experience first, making it streamlined the minute you order your car to when you receive it.'
-					/>
+				<div className='h-96 w-3/5 mx-auto my-2 p4 rounded-2xl bg-gradient-to-r from-purple-300 to-red-300'>
+				<div className='mx-3 my-3' style={{ width: '95%', height: '94%', position: 'relative', borderRadius: '10px', overflow: 'hidden' }}>
+						<Image className='object-none' src="/icons/car-image.jpeg" layout='fill'
+							objectFit='cover' />
+					</div>
 				</div>
 			</div>
 		);
